60a6a0037c602fdfab589a0034ace0578117d570
bcarlso1 <bcarlso1@gmail.com>
2020-04-18 15:11:14 -0400

    Currently showing Gif images as test

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React, {Component} from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 import Search from './Search';
 import Nav from './Nav';
 import PhotoContainer from './PhotoContainer';
-import apiKey from './config';
-import { BrowserRouter, Route, Link } from 'react-router-dom';
+// import apiKey from './config';
+// import { BrowserRouter, Route, Link } from 'react-router-dom';
 
 
  export default class App extends Component {
@@ -14,32 +14,39 @@ import { BrowserRouter, Route, Link } from 'react-router-dom';
     super();
     this.state = {
       pics: [],
-      loading: true
+      // loading: true
     };
   }
 
   componentDidMount() {
-      fetch(<apiKey />)
+      fetch('http://api.giphy.com/v1/gifs/search?q=${query}&limit=24&api_key=dc6zaTOxFJmzC')
+      // <apiKey />)
       .then(response => response.json())
       .then(responseData => {
-        this.setStste({ pics: responseData.data })
+        this.setState({ pics: responseData.data })
       })
+      .catch(error => {
+        console.log('Error fetching', error);
+      });
 
   }
 
+
+
   render() {
+    console.log(this.state.pics)
   return (
     <body>
-      <BrowserRouter>
-      <Route />
-      <Link />
+      {/* <BrowserRouter>
+      <Route /> */}
+      {/* <Link /> */}
       {/* add NavLink */}
       <div class="container">
         <Search />
         <Nav />
-        <PhotoContainer />
+        <PhotoContainer data={this.state.pics} />
       </div>
-      </BrowserRouter>
+      {/* </BrowserRouter> */}
     </body>
     );
   }
--- src/PhotoContainer.js
+++ src/PhotoContainer.js
@@ -1,26 +1,26 @@
-import React, {PureComponent} from 'react';
+import React from 'react';
 import NotFound from './NotFound';
 import Photo from './Photo';
 
 
-class PhotoContainer extends PureComponent {
+const PhotoContainer = props => {
 
-    render() {
-
-    
+    const results = props.data;
+    let pics = results.map(pic =>
+      <Photo url={pic.images.fixed_height_small.url} key={pic.id} />
+      );
     
     return (
         <div class="photo-container">
         <h2>Results</h2>
         <ul>
-        <Photo />
-        <NotFound />
-         
+          {pics}
         </ul>
+        <NotFound />
       </div>
-    )
+    );
    
-    };
 }
+
 
 export default PhotoContainer;
