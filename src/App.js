e5fdc07922f0bcaf2fd73829bd89beb402742df2
bcarlso1 <bcarlso1@gmail.com>
2020-04-30 23:45:52 -0400

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -2,44 +2,14 @@ import React, {Component} from 'react';
 import './App.css';
 import './index.css';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
-import apiKey from './Components/config.js';
 import Search from './Components/Search';
 import Nav from './Components/Nav';
 import PhotoContainer from './Components/PhotoContainer';
 import NotFound from './Components/NotFound';
 
-
+//  FIX LOADING
+// FIX SEARCH NOW THAT APP DOESNT lOAD PHOTOS
 export default class App extends Component {
-  
-  constructor() {
-    super();
-    this.state = {
-       pics: [],
-       loading: true,
-       title: "",
-       changeURL: ""
-            };
-  }
-
-  componentDidMount() {
-    this.performSearch();
-}
-
-  performSearch = (query="airplane") => 
-    fetch(`https://www.flickr.com/services/rest/?method=flickr.photos.search&api_key=${apiKey}&tags=${query}&per_page=24&format=json&nojsoncallback=1`)
-     
-    .then(response => response.json())
-     .then(responseData => {   
-            
-        this.setState({ 
-                pics: responseData.photos.photo,
-                loading: false,
-                title: "",
-            })
-          })
-       .catch(error => {
-          console.log(this.state.pics);
-        });
   
         render() {
     
@@ -48,15 +18,15 @@ export default class App extends Component {
            <BrowserRouter>
               <div class="container">
                   <Search onSearch={this.performSearch} />
-                  <Nav title="Nav" onSelect={this.performSearch} /> 
+                  <Nav title="Nav" /> 
                   <div>
-                      {
-                        (this.state.loading)
+                       {/*  {
+                       (this.state.loading)
                          ? <p>Loading...</p>
-                         :
+                         :  */}
                     <Switch>
-                    <Route exact path="/" render={ () => <PhotoContainer title="Default" data={this.state.pics} /> } />
-                    <Route exact path="/:id" render={ () => <PhotoContainer info={this.props} data={this.state.pics} /> } />
+                    <Route exact path="/" render={ (props) => <PhotoContainer {...props} id={props.match.params.id} /> } />
+                    <Route exact path="/:id" render={ () => <PhotoContainer /> } />
                     {/* <Route path="/mountain" render={ () => <PhotoContainer title="Mountain" data={this.state.pics} changeURL="mountain" /> } /> 
                     <Route path="/lake" render={ () => <PhotoContainer title="Lake" data={this.state.pics} /> } /> 
                     <Route path="/boat" render={ () => <PhotoContainer title="Boat" data={this.state.pics} /> } />  */}
@@ -64,7 +34,7 @@ export default class App extends Component {
                        {/* <Route path="/search/:query" */} 
                     <Route component={NotFound} /> */}
                     </Switch> 
-                      }
+                   //   }
               </div>
               </div>
             </BrowserRouter> 
