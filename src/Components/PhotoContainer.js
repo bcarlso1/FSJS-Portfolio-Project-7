563856f1ec1a6ca52f70f669761c94e09d9a23fb
bcarlso1 <bcarlso1@gmail.com>
2020-04-30 23:46:30 -0400

    Add files via upload

--- src/Components/PhotoContainer.js
+++ src/Components/PhotoContainer.js
@@ -1,13 +1,41 @@
 import React, {Component} from 'react';
 import NotFound from './NotFound';
 import Photo from './Photo';
+import apiKey from './config.js';
 
 export default class PhotoContainer extends Component {
-  
+ 
+  constructor() {
+    super();
+    this.state = {
+       pics: [],
+      // loading: true,
+       title: "",
+       changeURL: ""
+            };
+  }
   componentDidMount() {
-    console.log(this.props.match)
-      }
+    console.log(this.props);
+    // const { query } = this.props.computedMatch.url
+    this.performSearch();
     
+}
+
+  performSearch = (query="airplane") => 
+    fetch(`https://www.flickr.com/services/rest/?method=flickr.photos.search&api_key=${apiKey}&tags=${query}&per_page=24&format=json&nojsoncallback=1`)
+     
+    .then(response => response.json())
+     .then(responseData => {   
+            
+        this.setState({ 
+                pics: responseData.photos.photo,
+                loading: false,
+                title: "",
+            })
+          })
+       .catch(error => {
+          console.log(this.state.pics);
+        });
   
   // componentDidUpdate(prevProps) {
   //     if (this.props.changeURL !== prevProps.changeURL) {
@@ -18,7 +46,7 @@ export default class PhotoContainer extends Component {
 
       render() {
     
-     const results = this.props.data;
+     const results = this.state.pics;
      let pics;
       // console.log(this.props.match)
 
@@ -36,7 +64,7 @@ export default class PhotoContainer extends Component {
       return (
          <div class="photo-container">
          {/* <h2>{this.props.title} Results</h2> */}
-         {/* <h2>{this.props.match.params.id} Results</h2> */}
+         <h2>{id} Results</h2>
          <ul>
             {pics} 
          </ul>
